4cfaae93f78e8d4c5e4e67e9db0dcceeee5187b4
BasikaKoma <basika.koma@gmail.com>
2026-08-02 10:42:52 +0300

    Add landing/js/config.js

---
+++ landing/js/config.js
@@ -0,0 +1,7 @@
+window.LIFEV1_CONFIG = {
+  "supabaseUrl": "https://fxdnbepmiphyzebqdkyf.supabase.co",
+  "supabaseAnonKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4ZG5iZXBtaXBoeXplYnFka3lmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NjMxMTgsImV4cCI6MjA5NzEzOTExOH0.2mz8a4HJzAw2zynbgKFdlaids3egDR4wsVW2crCB2GQ",
+  "githubOwner": "BasikaKoma",
+  "githubRepo": "Lifev1",
+  "appName": "lifev1"
+};
